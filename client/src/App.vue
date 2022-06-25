be44e7388c1a604c6005face05abd37c6f18efe4
Eric Schirtzinger <eschirtzinger@gmail.com>
2022-06-25 19:13:36 +0000

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const items = [{id: '123', name: 'Swim' }, {id: '345', name: 'Garden' }]
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    My Sh*t
+  </h1>
+  <div v-for="item in items" :key="item.id">
+    <vue-feather type="feather"></vue-feather>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ client/src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import VueFeather from 'vue-feather'
+import App from './App.vue'
+import './index.css'
+
+const app = createApp(App);
+app.component(VueFeather.name, VueFeather);
+app.mount('#app');
